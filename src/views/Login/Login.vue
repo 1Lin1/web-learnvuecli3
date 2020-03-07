a63d9f4f0aabce5ffab844b5238c4805f8fce854
LIN <624965986@qq.com>
2020-03-07 13:15:32 +0800

    add

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,40 +1,33 @@
 <template>
-  <el-select v-model="value" placeholder="请选择">
-    <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value">
-    </el-option>
-  </el-select>
+  <div class="login-wrap" >
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+    </ul>
+  </div>
 </template>
 
 <script>
   export default {
     data() {
       return {
-        options: [{
-          value: '选项1',
-          label: '黄金糕'
-        }, {
-          value: '选项2',
-          label: '双皮奶'
-        }, {
-          value: '选项3',
-          label: '蚵仔煎'
-        }, {
-          value: '选项4',
-          label: '龙须面'
-        }, {
-          value: '选项5',
-          label: '北京烤鸭'
-        }],
-        value: ''
+      
       }
     }
   }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+  .login-wrap{
+    width: 1000px;
+    height: 1000px;
+    background-color: #82ccdd;
+  }
+  
 </style>
